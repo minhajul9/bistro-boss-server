ee276aa1c877baa31d55e17a7ab5bf6b857041b4
minhajul9 <110691447+minhajul9@users.noreply.github.com>
2023-05-30 18:11:10 +0600

    reviews link created

--- index.js
+++ index.js
@@ -31,10 +31,16 @@ async function run() {
 
 
         const database = client.db("bistroDB")
-        const menuCollection = database.collection('menu')
+        const menuCollection = database.collection('menu');
+        const reviewsCollection = database.collection('reviews');
 
         app.get('/menu', async(req, res) =>{
             const result = await menuCollection.find().toArray();
+            res.send(result)
+        })
+
+        app.get('/reviews', async(req, res) => {
+            const result = await reviewsCollection.find().toArray();
             res.send(result)
         })
 
